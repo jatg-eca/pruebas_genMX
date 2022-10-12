15173a48772faff9c6aa6c25977017f5710ac9bd
JA Gonzalez <jatgonzalez33@gmail.com>
2022-10-11 20:20:44 -0500

    Add: Full deck

--- reviewjavalanguage/src/classes/review/deck/FullDeck.java
+++ reviewjavalanguage/src/classes/review/deck/FullDeck.java
@@ -1,5 +1,30 @@
 package classes.review.deck;
 
 public class FullDeck {
+	
+	private Card card;
+	private String material;
+	private String coverArt;
+	
+	private static final int CARDS_DECK_NUMBER = 52;
+	
+	public FullDeck() {
+		Card abastos = new Card("A", "bastos", "negro");
+		Card aoros = new Card("A", "oros", "rojo");
+		Card acopas = new Card("A", "copas", "rojo");
+		Card aespadas = new Card("A", "espadas", "negro");
+		this.material = "papel";
+		this.coverArt = "rombos rojos";
+	}
+	
+	public void personalizeCoverArt(String coverArt) {
+		this.coverArt = coverArt;
+	}
+	
+	public void personalizeMaterial(String material) {
+		this.material = material;
+	}
+	
+	
 
 }
